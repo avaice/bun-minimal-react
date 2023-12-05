41a89967722f7a75959a5521c6fbf2d50704047c
avaice <dev@cho-ice.xyz>
2023-12-05 16:11:23 +0900

    feat: Implement dev mode

---
+++ run_dev.ts
@@ -0,0 +1,21 @@
+// for dev build
+
+await Bun.build({
+  entrypoints: ["./src/index.tsx"],
+  outdir: "./.cache",
+})
+
+Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url)
+    console.log(url.pathname)
+    if (url.pathname === "/index.js") {
+      return new Response(Bun.file("./.cache/index.js"))
+    }
+    if (url.pathname === "/") {
+      return new Response(Bun.file("./public/index.html"))
+    }
+    return new Response(Bun.file(`./public${url.pathname}`))
+  },
+})
